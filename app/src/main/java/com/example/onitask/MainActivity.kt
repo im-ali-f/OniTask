d3242f71a280a4bd17140312832a03611676cc77
unknown <alifarhad.work@gmail.com>
2024-01-22 00:29:52 -0800

    login/signup btns created

---
+++ app/src/main/java/com/example/onitask/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.onitask
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.onitask.ui.theme.OniTaskTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+@Composable
+fun LoginSignupContainer(){
+    Column(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+
+}
