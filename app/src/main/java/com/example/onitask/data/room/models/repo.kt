4c14fe91c2d033172f7857905e879e289b5c7192
unknown <alifarhad.work@gmail.com>
2024-01-23 04:52:19 -0800

    view model created

--- app/src/main/java/com/example/onitask/data/room/models/repo.kt
+++ app/src/main/java/com/example/onitask/data/room/models/repo.kt
@@ -1,7 +1,7 @@
 package com.example.onitask.data.room.models
 
 class repo(val db: db) {
-    suspend fun add1(account: Account){
+    suspend fun accManager(account: Account){
         db.accDAO().createUser(account)
         db.accDAO().getUser(0,"")
     }
--- app/src/main/java/com/example/onitask/repository/viewmodel.kt
+++ app/src/main/java/com/example/onitask/repository/viewmodel.kt
@@ -1,4 +1,15 @@
 package com.example.onitask.repository
 
-class viewmodel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.onitask.data.room.models.Account
+import com.example.onitask.data.room.models.repo
+import kotlinx.coroutines.launch
+
+class viewmodel(val repo: repo):ViewModel() {
+    fun userManage(data:Account){
+        viewModelScope.launch{
+            repo.accManager(data)
+        }
+    }
 }
